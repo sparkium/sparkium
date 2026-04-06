ef8c885bdfc2fe2cb508060341c55ef66a521f1c
Robin <git+r@obin.ch>
2026-04-06 12:43:53 +0000

    feat: adjust search bar

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'Sparkium',
+      components: {
+        Header: './src/overrides/Header.astro',
+      },
       customCss: ["./src/style.css"],
       logo: { src: './src/assets/logo.svg', alt: 'Sparkium Logo' },
       social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/sparkium' }, { icon: 'linkedin', label: 'LinkedIn', href: 'https://linkedin.com/company/sparkium' }],
