c0f12bda4a2cbc0343c039633794d4e1a4d14d61
Robin <git+r@obin.ch>
2026-04-13 20:08:34 +0000

    fix: virtual modules import

--- apps/website/src/env.d.ts
+++ apps/website/src/env.d.ts
@@ -1,4 +1,4 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="astro/client" />
-
-import "@astrojs/starlight/virtual";
-import "@astrojs/starlight/virtual-internal";
+/// <reference path="../node_modules/@astrojs/starlight/virtual.d.ts" />
+/// <reference path="../node_modules/@astrojs/starlight/virtual-internal.d.ts" />
